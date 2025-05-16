b80ea159da41a3283efdf78223323c9793846214
gubsitt <itsariya.d@ku.th>
2025-05-16 14:58:03 +0700

    หลายอย่าง

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const dbConfig = require("./config/dbConfig");
 const cors = require("cors");
 const {readdirSync} = require('fs');
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 
 readdirSync('./Routes').map((r) => app.use('/api', require(`./Routes/${r}`))); 
 
