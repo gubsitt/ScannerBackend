3fc34914cc6bc13841c6cecad7e0c3a2d749017e
gubsitt <itsariya.d@ku.th>
2025-05-29 10:28:22 +0700

    เพิ่ม search และ เพิ่มเงื่อนไข location

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {searchOrder,searchOrderDetails,searchScannedSN} = require('../controllers/search.controller');
+const {searchOrder,searchOrderDetails,searchScannedSN,searchStockBalance} = require('../controllers/search.controller');
 
 router.get('/search-orders',searchOrder );
 router.get('/search-orderdetails', searchOrderDetails);
 router.get('/search-scanned-sn', searchScannedSN);
+router.get('/search-product-changelocation',searchStockBalance );
 
 module.exports = router;
