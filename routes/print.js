89ea0e796f88e32efb2d6f928b5c1a8e1f4d3a25
gubsitt <itsariya.d@ku.th>
2025-06-11 10:23:30 +0700

    แก้ชื่อไฟล์ print

--- routes/print.js
+++ routes/print.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const printController = require('../controllers/print.contoller');
+const printController = require('../controllers/print.controller');
 
 router.post('/print', printController.printAndLog);
 
