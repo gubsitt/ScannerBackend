e1524c1e7418fcc4bf1165c000f51996797c33e3
gubsitt <itsariya.d@ku.th>
2025-06-26 09:26:27 +0700

    color neo

--- controllers/wprController.js
+++ controllers/wprController.js
@@ -22,7 +22,36 @@ exports.getWprData = async (req, res) => {
   FROM [WMS_NewWarehouse].[dbo].[View_WithdrawalProcessReqHead]
       WHERE F_WdProcessReqStatus = '1'
     `);
-        res.json(result.recordset);
+
+          
+    const today = new Date();
+    today.setHours(0,0,0,0);
+
+    function getColor(sendDate) {
+      if (!sendDate) return '';
+      const date = new Date(sendDate);
+      date.setHours(0,0,0,0);
+      const diff = Math.floor((date - today) / (1000 * 60 * 60 * 24));
+      if (diff < 0) return 'red';
+      if (diff === 0) return 'yellow';
+      if (diff === 1) return 'pink';
+      if (diff === 2) return 'blue';
+      if (diff === 3) return 'purple';
+      if (diff === 4) return 'lightsky';
+      if (diff === 5) return 'brown';
+      if (diff === 6) return 'lightgreen';
+      if (diff > 6) return 'green';
+      return '';
+    }
+
+      let orders = result.recordset.map(row => ({
+      ...row,
+      color: getColor(row.F_SendDate)
+  
+    }));
+
+
+        res.json(orders);
     } catch (err) {
         console.error('Error fetching WPR data:', err);
         res.status(500).json({ error: 'Internal Server Error' });
